d282bd35bac039c7ce9ebb2ec79511dc50a228c8
reshapeclub-tech <reshapeclub.tech@gmail.com>
2024-06-28 16:16:22 +0100

    added social shareo ption

--- js/scripts.js
+++ js/scripts.js
@@ -74,6 +74,41 @@
 
 		//Adding Repuso API Code Here
 		var script = document.createElement("script"); script.type = "module"; script.src = "https://repuso.com/widgets/2.0/rw-widget-masonry.js"; document.getElementsByTagName("head")[0].appendChild(script);
+
+		//Share the Page functionalities
+
+		const viewBtn = document.querySelector(".btn-calendar1"),
+		popup = document.querySelector(".popup"),
+		close = popup.querySelector(".close"),
+		field = popup.querySelector(".field"),
+		input = field.querySelector("input"),
+		copy = field.querySelector("button");
+	
+	viewBtn.onclick = (e) => {
+		e.preventDefault();
+		popup.classList.toggle("show");
+	}
+	close.onclick = () => {
+		viewBtn.click();
+	}
+	
+	copy.onclick = () => {
+		input.select(); //select input value
+		if (document.execCommand("copy")) { //if the selected text is copied
+			field.classList.add("active");
+			copy.innerText = "Copied";
+			setTimeout(() => {
+				window.getSelection().removeAllRanges(); //remove selection from page
+				field.classList.remove("active");
+				copy.innerText = "Copy";
+			}, 3000);
+		}
+	}
+	
+	document.querySelector('.backdrop').addEventListener("click", function(e) {
+		popup.classList.remove("show");
+	});
+
 	});
 
 
